231fc40595dfa5b5e4c90672a998fce381a67e63
fangpachara <110920172+fangpachara@users.noreply.github.com>
2025-07-27 11:29:47 +0700

    create leaveBalance

--- leaveSystem/src/main/java/com/example/leaveSystem/dto/LeaveBalanceModel.java
+++ leaveSystem/src/main/java/com/example/leaveSystem/dto/LeaveBalanceModel.java
@@ -2,5 +2,48 @@ package com.example.leaveSystem.dto;
 
 public class LeaveBalanceModel {
     private int Id;
+    private int userId;
+    private int leaveTypeId;
+    private int year;
+    private int remainingDays;
 
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getLeaveTypeId() {
+        return leaveTypeId;
+    }
+
+    public void setLeaveTypeId(int leaveTypeId) {
+        this.leaveTypeId = leaveTypeId;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getRemainingDays() {
+        return remainingDays;
+    }
+
+    public void setRemainingDays(int remainingDays) {
+        this.remainingDays = remainingDays;
+    }
 }
