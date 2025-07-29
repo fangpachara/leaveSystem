7f2c183629fd2639a8245ad0319c9ded410bc483
fangpachara <110920172+fangpachara@users.noreply.github.com>
2025-07-29 11:44:07 +0700

    update fontend

--- leaveFontend/src/app/leave-history/leave-history.component.ts
+++ leaveFontend/src/app/leave-history/leave-history.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-leave-history',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './leave-history.component.html',
   styleUrl: './leave-history.component.css'
 })
 export class LeaveHistoryComponent {
-
+  leaveHistory = [
+    {date: '5 ม.ค. 2568' , type: "ลาพักร้อน",day:10, status: "อนุมัติ"},
+    {date: '29 ก.ค. 2568' , type: "ลาป่วย",day:15, status: "อนุมัติ"}
+  ]
 }
--- leaveFontend/src/app/leave-request-form/leave-request-form.component.ts
+++ leaveFontend/src/app/leave-request-form/leave-request-form.component.ts
@@ -1,36 +1,25 @@
 import { Component } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatSelectModule} from '@angular/material/select';
 import { CommonModule } from '@angular/common';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatNativeDateModule } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-leave-request-form',
   standalone: true,
   imports: [
     CommonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-    MatSelectModule,
-    MatDatepickerModule,
     FormsModule,
     ReactiveFormsModule,
-    MatNativeDateModule
-],
+  ],
   templateUrl: './leave-request-form.component.html',
   styleUrl: './leave-request-form.component.css'
 })
 export class LeaveRequestFormComponent {
 
   leaveType = [
-    { id: 1, name: "ลาป่วย"},
-    { id: 2, name: "ลาพักร้อน"},
-    { id: 3, name: "ลากิจ"}
+    { id: 1, name: "ลาป่วย" },
+    { id: 2, name: "ลาพักร้อน" },
+    { id: 3, name: "ลากิจ" }
   ]
 
   formData = {
@@ -38,8 +27,44 @@ export class LeaveRequestFormComponent {
     startDate: '',
     endDate: '',
     status: 'รออนุมัติ',
-    reason: ''
+    reason: '',
+    leaveDay: 0
   }
 
 
+
+  calLeaveDate() {
+    const startDate = new Date(this.formData.startDate)
+    const endDate = new Date(this.formData.endDate)
+    if (endDate < startDate) {
+      alert("ใส่วันไม่ถูกต้อง")
+      this.formData.endDate = ''
+      return
+    }
+    if (startDate && endDate) {
+      const days = endDate.getTime() - startDate.getTime();
+      const day = Math.floor(days / (1000 * 3600 * 24) + 1)
+      this.formData.leaveDay = day
+    }
+
+  }
+
+  onSubmit() {
+    if (this.formData.leaveType || this.formData.startDate || this.formData.endDate || this.formData.reason) {
+      this.calLeaveDate()
+
+      console.log("submitted", this.formData)
+    }
+  }
+
+  onClear() {
+    this.formData = {
+      leaveType: '',
+      startDate: '',
+      endDate: '',
+      status: 'รออนุมัติ',
+      reason: '',
+      leaveDay: 0
+    }
+  }
 }
